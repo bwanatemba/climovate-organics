d5143ecdb2c1effa7978b49cffdd985f3db97f85
Edgar Temba <sakwaedgar57@gmail.com>
2025-01-21 19:36:13 +0300

    Link inport error fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Footer() {
   return (
@@ -6,8 +6,8 @@ export default function Footer() {
       <div className="container">
         <div className="row">
           <div className="col-md-6">
-            <p>&copy; 2023 Climovate Organics Ltd. All rights reserved.</p>
-            <p>Business Registration Number: 0123456789 (Registered in Kenya)</p>
+            <p>&copy; 2024 Climovate Organics Ltd. All rights reserved.</p>
+            <p>Business Registration Number: BRS-009879 (Registered in Kenya)</p>
           </div>
           <div className="col-md-6 text-md-end">
             <nav>
@@ -54,38 +54,5 @@ export default function Footer() {
         }
       `}</style>
     </footer>
-  )
-}
-
-import Link from "next/link"
-
-export default function Footer() {
-  return (
-    <footer className="bg-success text-white py-4 mt-auto">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <p>&copy; 2023 Climovate Organics Ltd. All rights reserved.</p>
-            <p>Business Registration Number: 0123456789 (Registered in Kenya)</p>
-          </div>
-          <div className="col-md-6 text-md-end">
-            <nav>
-              <ul className="list-inline">
-                <li className="list-inline-item">
-                  <Link href="/privacy" className="text-white">
-                    Privacy Policy
-                  </Link>
-                </li>
-                <li className="list-inline-item">
-                  <Link href="/terms" className="text-white">
-                    Terms of Service
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </div>
-    </footer>
-  )
+  );
 }
